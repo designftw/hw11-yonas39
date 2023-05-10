d0d431476767054262efe5f0ec5ffccf2fb1706b
yonas39 <124627215+yonas39@users.noreply.github.com>
2023-05-10 17:21:42 -0400

    submission ready 12 half-point

--- chat-client/chat-app/chat_1.js
+++ chat-client/chat-app/chat_1.js
@@ -140,17 +140,6 @@ const app = {
   },
 
   computed: {
-    // async translatedMessages() {
-    //   const translatedMessages = {};
-
-    //   for (const message of this.messages) {
-    //     translatedMessages[message.id] = await this.translateMessage(
-    //       message.content
-    //     );
-    //   }
-
-    //   return translatedMessages;
-    // },
     // ########################## Messages #################################
     // ########################## Messages #################################
     messages() {
@@ -225,37 +214,6 @@ const app = {
   },
 
   methods: {
-    // async translate() {
-    //   if (!this.textToTranslate) {
-    //     alert("Please enter text to translate.");
-    //     return;
-    //   }
-
-    //   try {
-    //     const response = await axios.post(
-    //       `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`,
-    //       {
-    //         q: this.textToTranslate,
-    //         target: this.targetLanguage,
-    //       }
-    //     );
-
-    //     if (
-    //       response.data &&
-    //       response.data.data &&
-    //       response.data.data.translations &&
-    //       response.data.data.translations.length > 0
-    //     ) {
-    //       this.translatedText =
-    //         response.data.data.translations[0].translatedText;
-    //     } else {
-    //       alert("Translation failed. Please try again.");
-    //     }
-    //   } catch (error) {
-    //     console.error("Error while translating:", error);
-    //     alert("Translation failed. Please try again.");
-    //   }
-    // },
     async translateMessage(messageContent) {
       if (!messageContent) {
         return messageContent;
